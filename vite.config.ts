ddfbb3d6fa1970559b19cce2a723405f181adef5
Kristo <christo.losier@gmail.com>
2026-07-10 13:29:53 +0200

    Configurer Vite pour GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,2 +1,7 @@
 import { defineConfig } from 'vite';
-export default defineConfig({});
+import react from '@vitejs/plugin-react';
+
+export default defineConfig(({ command }) => ({
+  plugins: [react()],
+  base: command === 'build' ? '/Tracker-Habit/' : '/',
+}));
