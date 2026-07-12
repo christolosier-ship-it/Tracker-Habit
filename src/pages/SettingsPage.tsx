945f89994e4fcc70d8b1d5f15e46f600ad59bc61
Kristo <christo.losier@gmail.com>
2026-07-13 01:05:27 +0200

    Créer la page Paramètres dédiée

---
+++ src/pages/SettingsPage.tsx
@@ -0,0 +1,31 @@
+import { PeriodControls } from "../features/period/PeriodControls";
+import { DataTransferPanel } from "../features/settings/DataTransferPanel";
+import { ThemeGallery } from "../features/settings/ThemeGallery";
+import { TrackingOptionsPanel } from "../features/settings/TrackingOptionsPanel";
+import { SettingsPageProps } from "./page-types";
+
+export function SettingsPage({
+  data,
+  setData,
+  setSettings,
+}: SettingsPageProps) {
+  return (
+    <>
+      <PeriodControls
+        year={data.settings.anneeActive}
+        month={data.settings.moisActif}
+        onYearChange={(year) => setSettings({ anneeActive: year })}
+        onMonthChange={(month) => setSettings({ moisActif: month })}
+      />
+      <ThemeGallery
+        activeThemeId={data.settings.themeId}
+        setSettings={setSettings}
+      />
+      <TrackingOptionsPanel
+        countMissingAsMissed={data.settings.compterNonSaisisCommeManques}
+        setSettings={setSettings}
+      />
+      <DataTransferPanel data={data} setData={setData} />
+    </>
+  );
+}
